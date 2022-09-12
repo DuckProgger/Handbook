ee4e67c656f4196eadba78df085b660a85db8f42
Виталий Никитин <nikitin-vitalii@rambler.ru>
2022-09-12 12:46:32 +1000

    Валидация модели.

--- Handbook/Shared/User.cs
+++ Handbook/Shared/User.cs
@@ -4,11 +4,26 @@ namespace Handbook.Shared;
 
 public class User
 {
+    private const string onlyLettersRegEx = @"^\p{L}+$";
+    private const string fieldRequiredMessage = "Обязательное поле.";
+    private const string onlyLettersValidateMessage = "Поле должно содержать только буквы.";
+
     public int Id { get; set; }
+
+    [Required(ErrorMessage = fieldRequiredMessage)]
+    [RegularExpression(onlyLettersRegEx, ErrorMessage = onlyLettersValidateMessage)]
     public string FirstName { get; set; } = string.Empty;
+   
+    [Required(ErrorMessage = fieldRequiredMessage)]
+    [RegularExpression(onlyLettersRegEx, ErrorMessage = onlyLettersValidateMessage)]
     public string LastName { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = fieldRequiredMessage)]
+    [RegularExpression(onlyLettersRegEx, ErrorMessage = onlyLettersValidateMessage)]
     public string Patronymic { get; set; } = string.Empty;
+    
     public ActiveDirectoryLogin? ActiveDirectoryLogin { get; set; }
+   
     public bool Active { get; set; }
 }
 
